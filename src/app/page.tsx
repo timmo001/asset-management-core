bfa1f41ccd7774819979c47f472d878ebb75c24b
Aidan Timson <contact@timmo.xyz>
2024-06-22 14:16:43 +0100

    Larger + more rows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
-
-const mockImages = [
+let mockImages = [
   "https://utfs.io/f/079fe122-f29c-4f53-9316-cc344ac5c82f-5nw3rt.png",
   "https://utfs.io/f/bcdf2cba-a07a-4d5d-bd26-e8db15a087ef-kc0ia5.png",
   "https://utfs.io/f/c4d353eb-91de-4970-b726-0d590cd5a3df-se5sys.png",
   "https://utfs.io/f/dafd034c-b1b3-4811-9151-e43c537d7014-e6hk9p.png",
 ];
+mockImages = [...mockImages, ...mockImages, ...mockImages, ...mockImages];
 
 export default function HomePage() {
   return (
@@ -16,7 +15,7 @@ export default function HomePage() {
         {mockImages.map((image, index) => (
           <div
             key={index}
-            className="w-48 transform shadow-lg transition duration-300 hover:scale-105"
+            className="w-64 transform shadow-lg transition duration-300 hover:scale-105"
           >
             <img src={image} className="rounded-lg" />
           </div>
