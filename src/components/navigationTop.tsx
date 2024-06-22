188a23da888df9ec92c4bb41cde27ab7a88458be
Aidan Timson <contact@timmo.xyz>
2024-06-22 14:43:02 +0100

    Add login button

--- src/components/navigationTop.tsx
+++ src/components/navigationTop.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavigationTop() {
   return (
-    <nav className="mx-auto flex w-full max-w-6xl items-center justify-start gap-4 p-4 text-lg font-light">
+    <nav className="mx-auto flex w-full max-w-6xl items-center justify-start gap-4 p-2 text-lg font-light">
       <div>
         <Link href="/">Asset Management</Link>
       </div>
@@ -10,6 +10,10 @@ export default function NavigationTop() {
       <div>
         <Link href="/">Lorem ipsum</Link>
       </div>
+      <div className="flex-1" />
+      <button className="transform rounded-md bg-violet-900 px-4 py-2 text-white shadow-lg drop-shadow-2xl transition duration-300 hover:scale-105">
+        <Link href="/login">Login</Link>
+      </button>
     </nav>
   );
 }
