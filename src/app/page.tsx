40d429fd006b38404acc4d97a0fcbfcdf7da9c88
Aidan Timson <contact@timmo.xyz>
2024-06-22 14:14:23 +0100

    Mock setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockImages = [
+  "https://utfs.io/f/079fe122-f29c-4f53-9316-cc344ac5c82f-5nw3rt.png",
+  "https://utfs.io/f/bcdf2cba-a07a-4d5d-bd26-e8db15a087ef-kc0ia5.png",
+  "https://utfs.io/f/c4d353eb-91de-4970-b726-0d590cd5a3df-se5sys.png",
+  "https://utfs.io/f/dafd034c-b1b3-4811-9151-e43c537d7014-e6hk9p.png",
+];
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Asset Management Core!
+    <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4 text-white">
+      <h1 className="text-5xl font-light drop-shadow-xl">Asset Management</h1>
+
+      <div className="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {mockImages.map((image, index) => (
+          <div
+            key={index}
+            className="w-48 transform shadow-lg transition duration-300 hover:scale-105"
+          >
+            <img src={image} className="rounded-lg" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
