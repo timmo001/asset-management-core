382c4cfbeb32679ab9de5e4d0639f24f11889093
Aidan Timson <contact@timmo.xyz>
2024-06-23 16:21:04 +0100

    Admin section

--- src/app/assets/[id]/page.tsx
+++ src/app/assets/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { notFound } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 import { getAsset } from "~/server/db/assets";
 import { getImagesByIdentifier } from "~/server/db/image";
+import { isCurrentUserAdmin } from "~/server/db";
 import SignedInContainer from "~/components/signedInContainer";
 
 export const dynamic = "force-dynamic";
@@ -14,6 +16,8 @@ export default async function Page({ params }: { params: { id: number } }) {
   const images = asset.imageIds
     ? await getImagesByIdentifier(`asset-${asset.id}`)
     : undefined;
+
+  const isUserAdmin = await isCurrentUserAdmin();
 
   return (
     <SignedInContainer>
@@ -36,6 +40,16 @@ export default async function Page({ params }: { params: { id: number } }) {
             />
           ))}
         </section>
+        {isUserAdmin && (
+          <section className="w-72">
+            <h2 className="pb-4 text-3xl font-semibold">Admin Section</h2>
+            <Link href={`/posts/${asset.id}/edit`}>
+              <button className="w-full transform rounded-lg bg-violet-900 px-7 py-2 font-normal transition duration-300 hover:bg-violet-800">
+                Edit Asset
+              </button>
+            </Link>
+          </section>
+        )}
       </>
     </SignedInContainer>
   );
