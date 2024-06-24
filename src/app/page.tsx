88ddc8c928a072289b099349a322154e0c2588ce
Aidan Timson <contact@timmo.xyz>
2024-06-24 10:02:50 +0100

    Add image to post on homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,33 @@ export default async function HomePage() {
       <section className="w-full">
         <h2 className="text-3xl font-semibold">News</h2>
         <div className="mt-2 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {posts.map((post) => (
-            <Link key={post.id} href={`/posts/${post.id}`}>
-              <div className="block rounded-lg bg-violet-950 bg-opacity-30 p-4 shadow-xl transition duration-300 hover:scale-105 hover:shadow-2xl">
-                <h3 className="text-xl font-medium">{post.title}</h3>
-                <p className="overflow-hidden truncate text-ellipsis text-wrap text-gray-500">
-                  {post.subtitle}
-                </p>
-              </div>
-            </Link>
-          ))}
+          {posts.map(async (post) => {
+            const image = await getImageByIdentifier(`post-${post.id}`);
+            return (
+              <Link key={post.id} href={`/posts/${post.id}`}>
+                <div className="relative h-52 transform bg-violet-950 bg-opacity-30 shadow-xl transition duration-300 hover:scale-105 hover:shadow-2xl">
+                  {image ? (
+                    <Image
+                      className="rounded-lg"
+                      src={image.url}
+                      alt={image.description || "Asset Image"}
+                      height={208}
+                      width={520}
+                      style={{ height: "13rem", objectFit: "cover" }}
+                    />
+                  ) : (
+                    <div className="h-40 w-full rounded-lg bg-gray-300"></div>
+                  )}
+                  <div className="absolute bottom-0 start-0 w-full rounded-bl-lg rounded-br-lg bg-slate-950 bg-opacity-40 px-2 py-2">
+                    <h3 className="text-lg font-medium">{post.title}</h3>
+                    <p className="overflow-hidden truncate text-ellipsis text-wrap text-sm text-gray-500">
+                      {post.subtitle}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
         </div>
       </section>
 
@@ -35,23 +52,20 @@ export default async function HomePage() {
           <section className="w-full">
             <h2 className="text-3xl font-semibold">Assets</h2>
             <div className="mt-2 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-              {assets.map(async (asset, index) => {
+              {assets.map(async (asset) => {
                 const image = await getImageByIdentifier(`asset-${asset.id}`);
                 return (
                   <Link key={asset.id} href={`/assets/${asset.id}`}>
-                    <div
-                      key={index}
-                      className="relative transform shadow-lg transition duration-300 hover:scale-105 hover:shadow-2xl"
-                    >
+                    <div className="relative h-52 transform bg-violet-950 bg-opacity-30 shadow-lg transition duration-300 hover:scale-105 hover:shadow-2xl">
                       {image ? (
                         <Image
                           className="rounded-lg"
                           src={image.url}
                           alt={image.description || "Asset Image"}
-                          height={520}
+                          height={208}
                           width={520}
-                          style={{ objectFit: "cover" }}
-                        />
+                          style={{ height: "13rem", objectFit: "cover" }}
+                          />
                       ) : (
                         <div className="h-40 w-full rounded-lg bg-gray-300"></div>
                       )}
