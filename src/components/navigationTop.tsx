ca2111b0962c48f43080029bdb17d437efbf12a9
Aidan Timson <contact@timmo.xyz>
2024-06-27 15:04:24 +0100

    Add breadcrumbs

--- src/components/navigationTop.tsx
+++ src/components/navigationTop.tsx
@@ -1,19 +1,50 @@
+"use client";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 import NewItem from "~/components/newItem";
 
+const paths: { [key: string]: string[] | undefined } = {
+  "/": [],
+  "/assets/new": ["Assets", "New Asset"],
+  "/assets/[id]/edit": ["Assets", "Asset", "Edit"],
+  "/assets/[id]": ["Assets", "Asset"],
+  "/assets": ["Assets"],
+  "/posts/new": ["Posts", "New Post"],
+  "/posts/[id]/edit": ["Posts", "Post", "Edit"],
+  "/posts/[id]": ["Posts", "Post"],
+  "/posts": ["Posts"],
+};
+
 export default function NavigationTop() {
+  const pathname = usePathname();
+
+  const breadcrumbs = useMemo(() => {
+    const path = Object.entries(paths).find(([r, t]) => {
+      const re = new RegExp(`^${r.replace(/\[.*?\]/g, ".*")}$`);
+      return re.test(pathname);
+    })?.[1];
+    if (!path) return null;
+    return path.map((t, i) => (
+      <>
+        <div>/</div>
+        <div key={i}>
+          {i === path.length - 1 ? <Link href={pathname}>{t}</Link> : t}
+        </div>
+      </>
+    ));
+  }, [pathname]);
+
   return (
     <nav className="mx-auto flex w-full max-w-screen-xl items-center justify-start gap-4 px-8 py-3 text-lg font-light">
       <div>
         <Link href="/">Asset Management</Link>
       </div>
-      <div>/</div>
-      <div>
-        <Link href="/">Lorem ipsum</Link>
-      </div>
+      {breadcrumbs}
       <div className="flex-1" />
+      {pathname}
       <div className="flex flex-row gap-4">
         <SignedOut>
           <div className="transform rounded-md bg-violet-900 bg-opacity-60 px-4 py-2 text-white shadow-lg drop-shadow-2xl transition duration-300 hover:scale-105">
